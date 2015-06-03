0d87016f206b9dab6c7a0d694db160dd07156499
Olivier Gayot <duskcoder@gmail.com>
2015-06-04 00:22:01 +0100

    removed useless content
    Signed-off-by: Olivier Gayot <duskcoder@gmail.com>

--- server.py
+++ server.py
@@ -13,9 +13,6 @@ def message_received_handler(client, server, message):
     print('received from client ' + client['id'] + ': [%s]' % message)
 
 def main():
-    white_cards = CAO_Cards.get_white_cards()
-    black_cards = CAO_Cards.get_black_cards()
-
     server = WebsocketServer(1236)
 
     server.set_fn_new_client(new_client_handler)
